d859b3c02cb73143694e085e367c2c4430b6d48b
squirrelAf <aleckraowo@gmail.com>
2025-05-01 20:11:34 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,58 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+   private:
+  struct Node {
+    T data;
+    Node* next;
+    explicit Node(T data, Node* next = nullptr) : data(data), next(next) {}
+  };
+
+  Node* front;
+
+ public:
+  TPQueue() : front(nullptr) {}
+
+  ~TPQueue() {
+    while (!isEmpty()) {
+      pop();
+    }
+  }
+
+  bool isEmpty() const { return front == nullptr; }
+
+  void push(const T& value) {
+    Node* nod = new Node(value);
+    if (isEmpty() || front->data.prior < value.prior) {
+      nod->next = front;
+      front = nod;
+    } else {
+      Node* current = front;
+      while (current->next && current->next->data.prior >= value.prior) {
+        current = current->next;
+      }
+      node->next = current->next;
+      current->next = nod;
+    }
+  }
+
+  T pop() {
+    if (isEmpty()) {
+      throw std::out_of_range("Queue is empty");
+    }
+    Node* temp = front;
+    front = front->next;
+    T data = temp->data;
+    delete temp;
+    return data;
+  }
+
+  T peek() const {
+    if (isEmpty()) {
+      throw std::out_of_range("Queue is empty");
+    }
+    return front->data;
+  }
 };
 
 struct SYM {
